4493cb2647d892deb567e423b8d4675a28520a23
sybbt <32449217+sybbt@users.noreply.github.com>
2017-10-05 16:14:24 +0200

    add(test)

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once 'function.php';
 
 $sUserAgent     = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36';
 $sUrlFacebook   = 'https://www.facebook.com/nytimes/photos/a.283559809998.33779.5281959998/10151320714864999/?type=1&theater';
+$sUrlTwitter    = 'https://twitter.com/HUPNVS/status/915934420732928001';
 $sUrlInsta      = 'https://www.instagram.com/p/BYqrvBfhNjP/';
 $urlFake        = 'http://docs.phpdoc.org/references/phpdoc/tags/global.html';
 $urlNotFound    = 'fgdgsdfgdfgdf.kom';
@@ -17,6 +18,9 @@ echo fPre(
 echo fPre(
   fGetInstagramPhotoPost($sUrlInsta)
 );
+echo fPre(
+  fGetTwitterPhotoPost($sUrlTwitter)
+);
 
 // Error 404
 echo 'ON ERROR 404';
@@ -25,6 +29,9 @@ echo fPre(
 );
 echo fPre(
   fGetInstagramPhotoPost($urlNotFound)
+);
+echo fPre(
+  fGetTwitterPhotoPost($urlNotFound)
 );
 
 // Error 202
@@ -35,5 +42,9 @@ echo fPre(
 echo fPre(
   fGetInstagramPhotoPost($urlFake)
 );
+echo fPre(
+  fGetTwitterPhotoPost($urlFake)
+);
+
 
 exit();
